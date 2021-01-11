2323c1038413956a1dbe46b8da2e2171c891a9e5
Pawel <choinskipawel2@gmail.com>
2021-01-11 23:07:17 +0100

    - added spotless plugin - created DriverFactory - formatting code

---
+++ src/test/java/org/pwl/framework/OverviewTabTests.java
@@ -0,0 +1,15 @@
+package org.pwl.framework;
+
+import static org.testng.Assert.*;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.*;
+
+public class OverviewTabTests extends BaseTestClass {
+    @Test
+    void usernameIsCorrectOnOverviewTab() {
+        driver.get(BASE_URL + USER);
+        String actualUsername = driver.findElement(By.className("p-nickname")).getText();
+        assertEquals(USER, actualUsername);
+    }
+}
