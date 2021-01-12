f6bce0886a2f2134f90248c90a97abe1c2ee00a7
Pawel <choinskipawel2@gmail.com>
2021-01-12 21:39:22 +0100

    - added IssuesTabTest

---
+++ src/test/java/org/pwl/framework/IssuesTabTest.java
@@ -0,0 +1,27 @@
+package org.pwl.framework;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class IssuesTabTest extends BaseTestClass {
+
+    @Test
+    void testIssuesFilters() {
+        driver.get(BASE_URL + USER + "/github-tests/issues");
+        WebElement closed = driver.findElement(By.partialLinkText("Close"));
+        System.out.println(closed.isDisplayed());
+        closed.click();
+        driver.findElement(By.id("author-select-menu")).click();
+        WebElement authorFilterField = driver.findElement(By.id("author-filter-field"));
+        authorFilterField.sendKeys(USER);
+        authorFilterField.sendKeys(Keys.ENTER);
+        driver.findElement(By.linkText("Organize tests")).click();
+        String currentUrl = driver.getCurrentUrl();
+        String expectedUrl = "https://github.com/pawelchoinski/github-tests/issues/1";
+        assertEquals(currentUrl, expectedUrl);
+    }
+}
